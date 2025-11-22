cd0a73f09dfbfeb24125ed945a85d927f7278861
Leonardo Esparis <ljesparis@gmail.com>
2025-11-23 00:01:14 +0100

    Add score
    drawing score and add speed based on 4 threshold. We yet don't increment the score

--- src/main.zig
+++ src/main.zig
@@ -136,14 +136,6 @@ const Entity = struct {
     handle: Handle = undefined,
 };
 
-const WWIDTH = 600;
-const WHEIGHT = 500;
-const MAX_ENTITIES = 1024;
-const MAX_TEXTURES = 2;
-
-const DINO_SCALING: f32 = 2.0;
-const CACTUS_SCALING: f32 = 1.6;
-
 fn Queries(entity_type: EntityType) type {
     return struct {
         fn getEntity(entity: *const Entity) bool {
@@ -159,8 +151,21 @@ fn Queries(entity_type: EntityType) type {
 const cactus_queries = Queries(.CACTUS);
 const dino_queries = Queries(.DINO);
 
-// CACTUS
 const CACTUS_SPAWN_DELAY: f32 = 2.0;
+const CACTUS_SCALING: f32 = 1.6;
+const CACTUS_SPEED: f32 = 200.0;
+const CACTUS_SPEED_INC: f32 = 50.0;
+
+fn getSpeedIncAccordingToScore(score: i32) f32 {
+    return -1 * switch (score) {
+        5 => CACTUS_SPEED + CACTUS_SPEED_INC * 2,
+        10 => CACTUS_SPEED + CACTUS_SPEED_INC * 3,
+        15 => CACTUS_SPEED + CACTUS_SPEED_INC * 4,
+        20 => CACTUS_SPEED + CACTUS_SPEED_INC * 5,
+        else => CACTUS_SPEED,
+    };
+}
+
 fn onCactusUpdate(game_state: *GameState, dt: f32) void {
     if (game_state.game_over) return;
 
@@ -179,7 +184,7 @@ fn onCactusUpdate(game_state: *GameState, dt: f32) void {
                     getScreenWidth() - width - (index * width * CACTUS_SCALING),
                     getScreenHeight() - height * CACTUS_SCALING,
                 ),
-                .velocity = .init(-200, 0),
+                .velocity = .init(getSpeedIncAccordingToScore(game_state.score), 0),
             });
         }
     }
@@ -220,16 +225,14 @@ fn onCactusDraw(game_state: *GameState) void {
         }
     }
 }
-// CACTUS
 
-// DINO
 const DINO_RUN_FRAMES: [2]u8 = .{ 24, 48 };
 const DINO_IDLE_FRAMES: [2]u8 = .{ 0, 72 };
 const DINO_GRAVITY: i32 = 2000;
-const DINO_JUMP: i32 = -650;
+const DINO_JUMP: i32 = -680;
 const DINO_TOTAL_FRAMES: i32 = 4;
 const DINO_ANIMATION_TIMEOUT: f32 = 0.1;
-
+const DINO_SCALING: f32 = 2.0;
 fn onDinoUpdate(game_state: *GameState, dt: f32) void {
     if (game_state.game_over) return;
 
@@ -260,7 +263,6 @@ fn onDinoUpdate(game_state: *GameState, dt: f32) void {
         grounded = true;
     }
 
-    // animation
     dino.animation_timer += dt;
     if (grounded and dino.animation_timer > DINO_ANIMATION_TIMEOUT) {
         if (dino.frame_index > DINO_RUN_FRAMES.len - 1) {
@@ -297,9 +299,7 @@ fn onDinoDraw(game_state: *GameState) void {
     );
     rl.drawTexturePro(texture, player_source, player_dest, .{ .x = 0, .y = 0 }, 0, .black);
 }
-// DINO
 
-// COLLISIONS
 fn onCollision(game_state: *GameState) void {
     const dino: *Entity = game_state.entities.getByQuery(dino_queries.getEntity).?;
     const dino_texture: rl.Texture2D = game_state.textures_2D.getByQuery(dino_queries.getResources).?.resource.texture_2d;
@@ -332,7 +332,6 @@ fn onCollision(game_state: *GameState) void {
         }
     }
 }
-// COLLISIONS
 
 fn drawCenteredText(text: [:0]const u8, font_size: i32, y: i32) void {
     const text_width: f32 = @floatFromInt(rl.measureText(text, font_size));
@@ -340,12 +339,16 @@ fn drawCenteredText(text: [:0]const u8, font_size: i32, y: i32) void {
     rl.drawText(text, x, WHEIGHT / 2 + y, font_size, .black);
 }
 
-// GAME STATE
+const WWIDTH = 600;
+const WHEIGHT = 500;
+const MAX_ENTITIES = 1024;
+const MAX_TEXTURES = 2;
 const GameState = struct {
     game_over: bool = false,
     entities: HandleMap(Entity, MAX_ENTITIES) = .{},
     textures_2D: HandleMap(Resource, MAX_TEXTURES) = .{},
 
+    score: i32 = 0,
     cactus_spawn_timer: f32 = 0.0,
     rand: std.Random = undefined,
 
@@ -400,6 +403,7 @@ const GameState = struct {
 
     fn reset(self: *Self) void {
         self.game_over = false;
+        self.score = 0;
         var it = self.entities.iterator();
         var entity = it.next();
         while (entity != null) : (entity = it.next()) {
@@ -414,8 +418,9 @@ const GameState = struct {
         }
     }
 
-    fn onDraw(self: *Self) void {
+    fn onDraw(self: *Self) !void {
         rl.clearBackground(.{ .r = 204, .g = 224, .b = 255, .a = 0 });
+        try self.drawScore();
         onDinoDraw(self);
         onCactusDraw(self);
 
@@ -424,9 +429,16 @@ const GameState = struct {
             drawCenteredText("Press 'space' key to restart the game", 20, 40);
         }
     }
+
+    fn drawScore(self: *Self) !void {
+        var buffer: [32:0]u8 = undefined;
+        const slice = try std.fmt.bufPrint(&buffer, "Score: {}", .{self.score});
+        buffer[slice.len] = 0;
+        const cstr: [:0]const u8 = buffer[0..slice.len :0];
+        rl.drawText(cstr, 10, 10, 20, .black);
+    }
 };
 
-// GAME STATE
 pub fn main() !void {
     rl.initWindow(WWIDTH, WHEIGHT, "google dino clone?");
     var game_state: GameState = try .init();
@@ -443,6 +455,6 @@ pub fn main() !void {
 
         rl.beginDrawing();
         defer rl.endDrawing();
-        game_state.onDraw();
+        try game_state.onDraw();
     }
 }
